210e9a46e5fc683c64ae3166c92e5ca9a5778a35
Kevin Ngo <ngo.kevin86@yahoo.com>
2020-05-21 14:44:36 -0700

    Initial commit

---
+++ testerMain.c
@@ -0,0 +1,51 @@
+/* Kevin Ngo
+ * C Tester
+ */
+
+/*
+	This program will run expecting either potentially one or no arguments.
+
+	If there are no arguments, then it will by default look for a specifically named textfile
+	named "input.txt" which will contain the exact comamnd to run.
+
+	There are optional input flags which will control what other functions to run
+
+	-g
+	generates makefile
+
+	-
+
+	
+
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+	//idea:
+	//be able to shortcut rerunning programs for testing.
+
+	/*
+	iteration one: just generate the makefile?
+
+	iteration two: read command from specific textfile
+
+	iteration three: pipe output to a file
+
+	iteration four: diff given output and expected output
+	
+
+
+
+	*/
+
+	//check input
+
+
+
+
+
+	return 0;
+}
