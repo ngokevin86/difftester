ecb4eb400272152f59fd13951d2dd34d1c69dfd7
Kevin Ngo <ngo.kevin86@yahoo.com>
2020-05-25 13:16:11 -0700

    Added some checks

--- testerMain.c
+++ testerMain.c
@@ -29,23 +29,38 @@ int main(int argc, char* argv[]){
 
 	/*
 	iteration one: just generate the makefile?
+		take program file name to generate
 
 	iteration two: read command from specific textfile
+		user defined textfile with input command which program will read and execute directly
 
 	iteration three: pipe output to a file
+		self explanitory, goes into "output.txt"
 
 	iteration four: diff given output and expected output
 	
-
-
-
 	*/
 
+	if(argc == 1){
+		//check for makefile
+		//check for program
+		//check for command textfile
+		//run command in textfile
+	}
+
 	//check input
+	if(argc > 2){	//no more than executable and potentially one flag
+		printf("Usage: './tester <-flag>'\n");
+		printf("Flags:\n");
+		printf("'-g': Generate Makefile\n");
+	}
+
+	//check argv[2]
 
 
+	printf("Usage: './tester <-flag>'\n");
+	printf("Flags: \n");
+	printf("'-g': Generate Makefile\n");
 
-
-
-	return 0;
+	return 1;
 }
