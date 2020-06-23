c55f85367da91a4ccc6addfc5e46e92fe7e414c2
Kevin Ngo <ngo.kevin86@yahoo.com>
2020-06-23 14:52:21 -0700

    Cleaned more comments, final of first version.

--- testerMain.c
+++ testerMain.c
@@ -16,12 +16,7 @@
 	A flag to allow diff against another text file (something other than output.txt)
 	-f <file.txt>
 	where 'file.txt' is the file which it will diff the output to.
-
-	-g
-	just generate a Makefile
-
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,24 +25,9 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-#define DEBUG 1
-
-//int makegen();
+#define DEBUG 0
 
 int main(int argc, char* argv[]){
-	/* 
-	Take in some number of arguments, with the first given argument being the name of the program executable
-	Execute the given program as given, just that it will pipe the output of that program to a .temp.txt file and then
-	at the end, diff that file and an expected output.txt
-	
-	Expected input
-	./tester <program> <arg1> <arg2> <arg3>...
-
-	-?later be able to give it a specific text file to compare against?
-		something like
-		./tester -f <textfile> <program> <arg1> <arg2> <arg3>...
-	*/
-
 	//check input
 	if(argc < 2 || argc > 5){
 		printf("Usage: './tester <program> <arg1> <arg2> <arg3>'\n");
@@ -71,16 +51,15 @@ int main(int argc, char* argv[]){
 	}
 
 	if(access(argv[argCount], F_OK) == -1){	//if program doesn't exist
-		printf("Error: program '%s' does not exist in current directory.\n", argv[argCount]);
+		printf("Error: program '%s' does not exist in directory.\n", argv[argCount]);
 		return 1;
 	}
-
 	if(access("output.txt", F_OK) != 0){	//if diff file doesn't exist
 		printf("Error: expected file 'output.txt' not found.\n");
 		return 1;
 	}
-	char* argExec[argc];
 
+	char* argExec[argc];
 	char* result;	//prepend "./" to the program name
 	if((result = malloc(strlen(argv[argCount]) + 3)) == NULL){
 		fprintf(stderr, "Error while mallocing: %s\n", strerror(errno));
@@ -95,11 +74,11 @@ int main(int argc, char* argv[]){
 	}
 	argExec[argc - 1] = NULL;
 
-	if(DEBUG){
-		for(int i = 0; i < argc; i++){
-			printf("argExec: %s\n", argExec[i]);
-		}
+	printf("Going to execute: ");
+	for(int i = 0; i < argc - 1; i++){
+		printf("%s ", argExec[i]);
 	}
+	printf("\n");
 
 	//create pipe
 	int fd[2];
@@ -137,9 +116,19 @@ int main(int argc, char* argv[]){
 		char* argDiff[3];
 		argDiff[0] = "diff";
 		argDiff[1] = ".temp.txt";
+		/*
+		if -f flag is set
+			argDiff[2] = argv[3]
+		else
+			argDiff[2] = "output.txt"
+		*/
 		argDiff[2] = "output.txt";
 		argDiff[3] = NULL;
-		execvp(argDiff[0], argDiff);
+		printf(">>>>Diffing between output from '%s' and output.txt...\n", argv[argCount]);
+		if((execvp(argDiff[0], argDiff)) == -1){
+			fprintf(stderr, "Error exec in parent: %s\n", strerror(errno));
+			exit(errno);
+		}
 	}
 	else if(errno != 0){	//error
 		fprintf(stderr, "Error while forking: %s\n", strerror(errno));
@@ -151,21 +140,14 @@ int main(int argc, char* argv[]){
 			fprintf(stderr, "Error closing read pipe: %s\n", strerror(errno));
 			exit(errno);
 		}
-		//point stdout to pipe
-		if((dup2(fdWrite, 1)) == -1){
+		if((dup2(fdWrite, 1)) == -1){	//set stdout to pipe
 			fprintf(stderr, "Error dup2 (stdout) in child: %s\n", strerror(errno));
 			exit(errno);
 		}
-		/*if((dup2(fdWrite, 2)) == -1){
+		if((dup2(fdWrite, 2)) == -1){	//set stderr to pipe
 			fprintf(stderr, "Error dup2 (stderr) in child: %s\n", strerror(errno));
 			exit(errno);
-		}*/
-		
-		//exec program
-		//argExec[] = {argv[argCount], argv[argCount + 1], argv[argCount + 2], NULL};	//NULL needs to be last argument
-		//needs to be dynamic dependent on how many arguments are supplied
-		//which is more important when there are more than just 5 arguments
-		//although a program that takes more than five arguments would just be insane
+		}
 		if((execvp(argExec[0], argExec)) == -1){
 			fprintf(stderr, "Error exec in child: %s\n", strerror(errno));
 			exit(errno);
@@ -174,41 +156,3 @@ int main(int argc, char* argv[]){
 	}
 	return 0;
 }
-
-/*
-int makegen(){
-	//make sure makefile doesn't already exist
-	if(access("Makefile", F_OK) != -1){
-		printf("Makefile already exists in current directory, move or remove before running.\n");
-		return 1;
-	}
-
-	//prompt user for file name
-	//?prompt user for executable name
-	printf("Enter the name of the program to be compiled (do not include the '.c' file extension):\n");
-	//read input
-		//perhaps cap the amount of input, and put in a placeholder instead so user can modify it themselves?
-
-	//create file "Makefile"
-
-
-	//write commented header followed by makefile commands
-	//something like
-
-	//#<program name> Makefile
-	//all: <program name>.c
-	//	gcc -o <program name> <program name.c>
-	//run: <program name>
-	//	./<program name>
-	//clean:
-	//	rm -f <program name> *.o
-
-	
-	//alternatively I should just make it use dollar replacement, which means all, run, and clean will be the same between
-	//all generated makefiles, with only the line with the replacement being different
-	//--probably just add this later as a refactorization
-
-
-
-	return 0;
-}*/
